6173609ab764c16909b507abb4498d8a2c8166c4
Sebastian <parra.sebastian91@gmail.com>
2026-05-19 11:51:28 -0400

    se agrega ofertador

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { initFederation } from '@angular-architects/native-federation';
 initFederation({
   'seis-mfe-gestion-usuario': '/mfe-gestion-usuario/remoteEntry.json',
   'seis-mfe-dashboard-facturas': '/mfe-dashboard-facturas/remoteEntry.json',
-  'seis-mfe-publicador-facturas': '/mfe-publicador-facturas/remoteEntry.json'
+  'seis-mfe-publicador-facturas': '/mfe-publicador-facturas/remoteEntry.json',
+  'seis-mfe-ofertador-facturas': '/mfe-ofertador-facturas/remoteEntry.json'
 })
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
