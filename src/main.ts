fcb1994f72757ed402cc4df19648d76734b3eda1
Sebastian Parra <sparra@imed.cl>
2026-06-03 14:21:17 -0400

    feat(HU-12): initFederation carga manifest desde archivo, puertos Docker en dev manifest

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import { initFederation } from '@angular-architects/native-federation';
 
-initFederation({
-  'seis-mfe-gestion-usuario': '/mfe-gestion-usuario/remoteEntry.json',
-  'seis-mfe-dashboard-facturas': '/mfe-dashboard-facturas/remoteEntry.json',
-  'seis-mfe-publicador-facturas': '/mfe-publicador-facturas/remoteEntry.json',
-  'seis-mfe-ofertador-facturas': '/mfe-ofertador-facturas/remoteEntry.json'
-})
-  .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
+initFederation('/federation.manifest.json')
+  .catch(err => console.error('Error cargando federation manifest:', err))
+  .then(() => import('./bootstrap'))
   .catch(err => console.error(err));
